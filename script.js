160a5525b6bcafb57b81842cf4701bc3bbe27d2f
Vladyslav Pliasetskyi <62838901+vpliasetskyi@users.noreply.github.com>
2026-02-23 19:45:08 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("magic-button");
+const box = document.getElementById("message-box");
+// 2. Listen for a click on the button
+button.addEventListener("click", function() {
+// 3. When clicked, run this function:
+changeMessage();
+});
